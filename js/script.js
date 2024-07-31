64107f8160dfd5798e821fc6e2909bb318dcae78
Augusto Marques de Souza <augustomarquesdesouza@gmail.com>
2024-07-31 13:46:29 -0300

    Integração do desafio do accordion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+// Navbar Icon
 let changeIcon = function(icon) {
-    icon.classList.toggle('bi-x')
+    icon.classList.toggle('bi-x');
 }
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+const vejaMaisButton = document.getElementById('vejaMais');
+
+let accordionIsOpen = false; 
+
+accordionItems.forEach((item, index) => {
+    if (index >= 3) {
+        item.style.display = 'none';
+    }
+});
+
+function accordionShow() {
+    accordionItems.forEach((item, index) => {
+        if (index >= 3) {
+            item.style.display = 'flex';
+        }
+    });
+    accordionIsOpen = true; 
+};
+
+function accordionClose() {
+    accordionItems.forEach((item, index) => {
+        if (index >= 3) {
+            item.style.display = 'none';
+        }
+    });
+    accordionIsOpen = false; 
+};
+
+vejaMaisButton.addEventListener('click', function() {
+    if (accordionIsOpen) { 
+        accordionClose();
+    } else {
+        accordionShow();
+    }
+});
